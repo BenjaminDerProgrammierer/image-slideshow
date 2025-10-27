cb845bb1a798f9ea7cf944219411aeb2e0c133ee
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2025-10-27 12:43:24 +0100

    Add a label

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const slideshowElement = document.getElementById('slideshow');
 const imageElement = document.getElementsByClassName('slideshow-image')[0];
+const captionElement = document.getElementById('slideshow-caption');
 
 // Ten sample images from a placeholder service
 // const images = [
@@ -17,16 +18,16 @@ const imageElement = document.getElementsByClassName('slideshow-image')[0];
 
 // Ten sushi images from unsplash.com
 const images = [
-    { url: 'images/david-foodphototasty-8nFtscE1D_w-unsplash.jpg', alt: 'Image 1', },
-    { url: 'images/douglas-lopez-vlx0BwZlUXQ-unsplash.jpg', alt: 'Image 2' },
-    { url: 'images/jakub-dziubak-iOHJKJqO6E0-unsplash.jpg', alt: 'Image 3' },
-    { url: 'images/kyle-head-PW8K-W-Kni0-unsplash.jpg', alt: 'Image 4' },
-    { url: 'images/mahmoud-fawzy-Dbx6-XZY6Dg-unsplash.jpg', alt: 'Image 5' },
-    { url: 'images/mahmoud-fawzy-EqoCUzG9200-unsplash.jpg', alt: 'Image 6' },
-    { url: 'images/mahmoud-fawzy-MgvByrhYz24-unsplash.jpg', alt: 'Image 7' },
-    { url: 'images/mahmoud-fawzy-n1DePkKznLY-unsplash.jpg', alt: 'Image 8' },
-    { url: 'images/riccardo-bergamini-O2yNzXdqOu0-unsplash.jpg', alt: 'Image 9' },
-    { url: 'images/vinicius-benedit--1GEAA8q3wk-unsplash.jpg', alt: 'Image 10' }
+    { url: 'images/david-foodphototasty-8nFtscE1D_w-unsplash.jpg', alt: 'Sushi Rolls on a plate', },
+    { url: 'images/douglas-lopez-vlx0BwZlUXQ-unsplash.jpg', alt: 'A Nigiri being held with chopsticks' },
+    { url: 'images/jakub-dziubak-iOHJKJqO6E0-unsplash.jpg', alt: 'A variety of sushi on a stone board' },
+    { url: 'images/kyle-head-PW8K-W-Kni0-unsplash.jpg', alt: 'A Chef preparing sushi' },
+    { url: 'images/mahmoud-fawzy-Dbx6-XZY6Dg-unsplash.jpg', alt: 'Different Nigiri on a plate' },
+    { url: 'images/mahmoud-fawzy-EqoCUzG9200-unsplash.jpg', alt: 'California Rolls on a plate' },
+    { url: 'images/mahmoud-fawzy-MgvByrhYz24-unsplash.jpg', alt: 'A Sushi Roll being held by chopsticks' },
+    { url: 'images/mahmoud-fawzy-n1DePkKznLY-unsplash.jpg', alt: 'Rolls on a Plate' },
+    { url: 'images/riccardo-bergamini-O2yNzXdqOu0-unsplash.jpg', alt: 'A variety of sushi on a wooden board' },
+    { url: 'images/vinicius-benedit--1GEAA8q3wk-unsplash.jpg', alt: 'California Rolls' }
 ];
 
 let currentIndex = 0;
@@ -43,6 +44,7 @@ function showImage(image) {
     slideshowElement.style.setProperty('--bg-image', `url("${image.url}")`);
     imageElement.src = image.url;
     imageElement.alt = image.alt;
+    captionElement.textContent = image.alt;
 }
 
 function incrementIndex() {
